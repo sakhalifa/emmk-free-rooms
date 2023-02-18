24bfebdb18c604035d3aa3006e8664bccc963f21
Khalifa Samuel <samuel.khalifa@bordeaux-inp.fr>
2023-02-18 22:58:53 +0100

    added cache cleanup and migrated from object to map

--- src/lib/server/cache.ts
+++ src/lib/server/cache.ts
@@ -1,4 +1,5 @@
 import waitUntil from "async-wait-until"
+import { CLEAN_TIMER } from "$env/static/private"
 
 type CacheElement = {
 	data?: any,
@@ -8,14 +9,25 @@ type CacheElement = {
 	ttl: number
 }
 
-const cache: { [key: string]: CacheElement } = {}
+const cache: Map<string, CacheElement> = new Map<string, CacheElement>()
+
+const cleaner = setInterval(() => {
+	const toDelete = new Set<string>()
+	for (const [k, v] of cache) {
+		if (!v.isFetching && Date.now() - v.ttl * 2 > v.lastUpdate)
+			toDelete.add(k)
+	}
+	for (const cacheKey of toDelete) {
+		cache.delete(cacheKey)
+	}
+}, Number.parseInt(CLEAN_TIMER))
 
 function _isExpired(el: CacheElement) {
 	return !el.isFetching && Date.now() - el.ttl > el.lastUpdate
 }
 
 async function _update(key: string) {
-	const value = cache[key]
+	const value = cache.get(key)!
 	value.isFetching = true
 	let data = await value.fetcher(key)
 	value.isFetching = false
@@ -30,16 +42,16 @@ async function _update(key: string) {
  * @param fetcher function used to fetch new data from key
  */
 async function getOrRevalidate(key: string, ttl: number = 600 * 1000, fetcher?: (key: string) => Promise<any>) {
-	if (cache[key] === undefined) {
+	if (cache.get(key) === undefined) {
 		const value: CacheElement = {
 			fetcher: fetcher ? fetcher : (key) => fetch(key).then((r) => r.json()),
 			isFetching: false,
 			lastUpdate: 0,
 			ttl: ttl,
 		}
-		cache[key] = value;
+		cache.set(key, value);
 	}
-	const value = cache[key]
+	const value = cache.get(key)!
 	if (_isExpired(value))
 		_update(key)
 	if (value.isFetching)
