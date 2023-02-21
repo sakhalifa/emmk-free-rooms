42767bd6c7335f955b8a3799b7f4b1db7ce1fcbf
Khalifa Samuel <samuel.khalifa@bordeaux-inp.fr>
2023-02-21 12:05:59 +0100

    skeleton for room planning

---
+++ src/routes/rooms/[id]/+page.server.js
@@ -0,0 +1,17 @@
+import { getRooms } from '$lib/server/roomStore';
+import { error } from '@sveltejs/kit';
+
+/** @type {import('./$types').PageServerLoad} */
+export async function load({params}){
+	const idNum = Number(params.id)
+	if(!Number.isInteger(idNum))
+		throw error(400, "Invalid id! Must be an integer.")
+	const rooms = await getRooms()
+	let room = rooms.find((r) => r.id === idNum)
+	console.log()
+	if(room === undefined)
+		throw error(400, `Cannot find room with id ${params.id}`)
+	return {
+		room
+	}
+}
---
+++ src/routes/rooms/[id]/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import MyCalendar from '$lib/MyCalendar.svelte';
+
+	/** @type import('./$types').PageData*/
+	export let data;
+
+	function getUrl(){}
+</script>
+
+<h1>{data.room.name}</h1>
+<!-- <MyCalendar {fetchEvents} /> -->
