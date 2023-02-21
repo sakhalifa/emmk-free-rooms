5d1eecbaf5bd285278b0a37b69dfb765ca42e491
Khalifa Samuel <samuel.khalifa@bordeaux-inp.fr>
2023-02-21 12:05:47 +0100

    more refactor yay

--- src/lib/MyCalendar.svelte
+++ src/lib/MyCalendar.svelte
@@ -6,10 +6,68 @@
 </script>
 
 <script>
-	/** @type {(fetchInfo: {start: Date, end: Date}, successCallback: (_: any[]) => void) => void}*/
-	export let fetchEvents
+	/** @type (s: Date, e: Date) => string */
+	export let getUrl;
+	/**
+	 *
+	 * @param {string | null} type
+	 */
+	function getColorFromType(type) {
+		switch (type) {
+			case 'CM':
+				return 'yellow';
+			case 'CI':
+				return 'lime';
+			case 'TD':
+				return 'cyan';
+			case 'ENSEIGNANTS':
+				return 'red';
+			case 'TP':
+				return 'lightgray';
+			default:
+				return '';
+		}
+	}
+	/**
+	 *
+	 * @param {{start: Date, end: Date}} fetchInfo
+	 * @param {(_: any[]) => void} successCallback
+	 */
+	function fetchEvents(fetchInfo, successCallback) {
+		let { start, end } = fetchInfo;
+		start = new Date(start);
+		end = new Date(end);
+		if (start.getDay() === 0) {
+			start.setDate(start.getDate() + 1);
+			end.setDate(end.getDate() + 1);
+		}
+		fetch(getUrl(start, end))
+			.then((r) => r.json())
+			.then((d) => {
+				/** @type import('$lib/types.d').PlanningOfDay[]*/
+				const plannings = d;
+				const events = [];
+				for (const planning of plannings) {
+					for (const ev of planning.events) {
+						events.push({
+							id: ev.description.id,
+							start: new Date(ev.start),
+							end: new Date(ev.end),
+							titleHTML: `<span class="title-bold">${ev.summary}</span><br>${
+								ev.location?.replaceAll(',', '<br>') ?? ''
+							}`,
+							color: getColorFromType(ev.description.type),
+							extendedProps: {
+								type: ev.description.type
+							}
+						});
+					}
+				}
+				successCallback(events);
+			});
+	}
 	/** @type {HTMLDivElement} */
-	let tooltipRef
+	let tooltipRef;
 
 	const activeTooltipStyle = {
 		position: 'absolute',
@@ -76,9 +134,8 @@
 </script>
 
 <div class="tooltip" bind:this={tooltipRef}>
-	<div class="summary"></div>
-	<div class="content">
-	</div>
+	<div class="summary" />
+	<div class="content" />
 </div>
 
 <Calendar {options} {plugins} />
--- src/routes/users/[login]/+page.svelte
+++ src/routes/users/[login]/+page.svelte
@@ -9,68 +9,15 @@
 	export let data;
 
 	/**
-	 *
-	 * @param {string | null} type
+	 * 
+	 * @param {Date} start
+	 * @param {Date} end
 	 */
-	function getColorFromType(type) {
-		switch (type) {
-			case 'CM':
-				return 'yellow';
-			case 'CI':
-				return 'lime';
-			case 'TD':
-				return 'cyan';
-			case 'ENSEIGNANTS':
-				return 'red';
-			case 'TP':
-				return 'lightgray';
-			default:
-				return '';
-		}
+	function getUrl(start, end){
+		return `/api/users/${data.user.login}?start=${convertDateToISODay(start)}&end=${convertDateToISODay(end)}`
 	}
 
-	/**
-	 *
-	 * @param {{start: Date, end: Date}} fetchInfo
-	 * @param {(_: any[]) => void} successCallback
-	 */
-	function fetchEvents(fetchInfo, successCallback) {
-		let { start, end } = fetchInfo;
-		start = new Date(start);
-		end = new Date(end);
-		if (start.getDay() === 0) {
-			start.setDate(start.getDate() + 1);
-			end.setDate(end.getDate() + 1);
-		}
-		fetch(
-			`/api/users/${data.user?.login ?? 'a'}?start=${convertDateToISODay(
-				start
-			)}&end=${convertDateToISODay(end)}`
-		)
-			.then((r) => r.json())
-			.then((d) => {
-				/** @type import('$lib/types.d').PlanningOfDay[]*/
-				const plannings = d;
-				const events = [];
-				for (const planning of plannings) {
-					for (const ev of planning.events) {
-						events.push({
-							id: ev.description.id,
-							start: new Date(ev.start),
-							end: new Date(ev.end),
-							titleHTML: `<span class="title-bold">${ev.summary}</span><br>${
-								ev.location?.replaceAll(',', '<br>') ?? ''
-							}`,
-							color: getColorFromType(ev.description.type),
-							extendedProps: {
-								type: ev.description.type
-							}
-						});
-					}
-				}
-				successCallback(events);
-			});
-	}
+	
 </script>
 
-<MyCalendar {fetchEvents} />
+<MyCalendar {getUrl} />
