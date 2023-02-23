1c014b97b67d258d90944a3d9287a52ab1260b0b
Khalifa Samuel <samuel.khalifa@bordeaux-inp.fr>
2023-02-23 15:16:18 +0100

    fix bug

--- src/routes/users/+page.svelte
+++ src/routes/users/+page.svelte
@@ -1,10 +1,13 @@
 <script>
-	import { goto } from "$app/navigation";
+	import { browser } from '$app/environment';
+	import { goto } from '$app/navigation';
 
 	/** @type import('./$types').PageData*/
 	export let data;
 
-	goto(`/users/${data.session.user.user}`)
+	if (browser) {
+		goto(`/users/${data.session.user.user}`);
+	}
 </script>
 
 <p>Page is loading...</p>
